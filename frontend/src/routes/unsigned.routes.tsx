c596cf7cacc04d8d905e621eb1045c016bdeb756
Kael Silva <kaelsilvaaraujo@gmail.com>
2023-03-30 22:02:34 -0300

    feat: add product page

--- frontend/src/routes/unsigned.routes.tsx
+++ frontend/src/routes/unsigned.routes.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "../pages/Home";
+import Product from "../pages/Product";
 
 const UnsignedRoutes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/product/:id" component={Product} />
     <Redirect to="/" />
   </Switch>
 );
