ce28f1742d55f836116a8cddb8f918890d948e86
Kael Silva <kaelsilvaaraujo@gmail.com>
2023-04-01 20:47:15 -0300

    feat: add order by selection

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -8,10 +8,26 @@ const Home: React.FC = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [categories, setCategories] = useState<string[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [selectedOrderBy, setSelectedOrderBy] = useState<string>("");
   const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([]);
 
-  function handleChange(event: React.ChangeEvent<HTMLSelectElement>): void {
+  const orderByOptions = [
+    "Nenhum",
+    "Nome",
+    "Valor crescente",
+    "Valor decrescente",
+  ];
+
+  function handleChangeCategory(
+    event: React.ChangeEvent<HTMLSelectElement>
+  ): void {
     setSelectedCategory(event.target.value);
+  }
+
+  function handleChangeOrderBy(
+    event: React.ChangeEvent<HTMLSelectElement>
+  ): void {
+    setSelectedOrderBy(event.target.value);
   }
 
   useEffect(() => {
@@ -23,27 +39,69 @@ const Home: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    const newFilteredProducts = selectedCategory ? products.filter(
-      (product) => product.category === selectedCategory
-    ) : products;
+    if (selectedCategory && selectedCategory !== "Nenhum filtro") {
+      const newFilteredProducts = selectedCategory
+        ? products.filter((product) => product.category === selectedCategory)
+        : products;
 
-    setFilteredProducts(newFilteredProducts);
+      setFilteredProducts(newFilteredProducts);
+    }
+
+    setSelectedOrderBy("");
   }, [selectedCategory]);
+
+  useEffect(() => {
+    switch (selectedOrderBy) {
+      case "Nenhum":
+        setFilteredProducts(products);
+        setSelectedCategory("");
+        break;
+      case "Nome":
+        setFilteredProducts((prevFilteredProducts) =>
+          [...prevFilteredProducts].sort((a, b) =>
+            a.title.localeCompare(b.title)
+          )
+        );
+        break;
+      case "Valor crescente":
+        setFilteredProducts((prevFilteredProducts) =>
+          [...prevFilteredProducts].sort((a, b) => a.value - b.value)
+        );
+        break;
+      case "Valor decrescente":
+        setFilteredProducts((prevFilteredProducts) =>
+          [...prevFilteredProducts].sort((a, b) => b.value - a.value)
+        );
+        break;
+      default:
+        setFilteredProducts(products);
+        break;
+    }
+  }, [selectedOrderBy]);
 
   return (
     <Container>
       <h1>Home</h1>
 
-      <Select onChange={handleChange}>
-        <option value="" disabled selected hidden>
+      <Select onChange={handleChangeCategory} value={selectedCategory}>
+        <option value="" disabled hidden>
           Filtre por categoria
         </option>
-        <option value="">
-          Nenhum filtro
-        </option>
+        <option value="">Nenhum filtro</option>
         {categories.map((category) => (
           <option key={category} value={category}>
             {category}
+          </option>
+        ))}
+      </Select>
+
+      <Select onChange={handleChangeOrderBy} value={selectedOrderBy}>
+        <option value="" disabled hidden>
+          Ordernar por...
+        </option>
+        {orderByOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
           </option>
         ))}
       </Select>
