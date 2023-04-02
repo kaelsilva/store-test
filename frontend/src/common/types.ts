0cfeee4f91761fa75ada0df60a3ccebddefb4afe
Kael Silva <kaelsilvaaraujo@gmail.com>
2023-04-02 08:43:52 -0300

    feat: add cart and cart item interfaces and update product interface with methods for add and remove item and identifier if item is in cart

--- frontend/src/common/types.ts
+++ frontend/src/common/types.ts
@@ -1,8 +1,23 @@
-// This is an product interface
+/** This is an product interface*/
 export interface IProduct {
-  id?: string;
+  id: string;
   title: string;
   value: number;
   category: string;
   imageUrl: string;
+  itemInCart: boolean;
+  addItemToCart: ({}: ICartItem) => void;
+  removeItemFromCart: (itemId: string) => void;
+}
+
+/** This is an cart item interface, which 'itemId' represents 'id' and 'itemValue' represents 'value' from IProduct*/
+export interface ICartItem {
+  itemId?: string;
+  itemValue: number;
+}
+
+/** This is an cart interface, which contains a list of itens and total value represents the sum of the values from the itens*/
+export interface ICart {
+  items: ICartItem[];
+  totalValue: number;
 }
