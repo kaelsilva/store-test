a9602d2f70e857cf271869e8fa45f63353c804f6
Kael Silva <kaelsilvaaraujo@gmail.com>
2023-04-04 21:09:40 -0300

    fix: remove db port from mongodb uri

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connection = mongoose.connect(
-  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`
 );
 
 export default connection;
